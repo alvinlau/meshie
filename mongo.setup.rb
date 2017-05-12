b49f26ea371e0064e191855423c10134f8c73555
Alvin Lau <mail@alvinlau.me>
2017-05-11 21:41:06 -0700

    delete useless line in mongo setup file

--- mongo.setup.rb
+++ mongo.setup.rb
@@ -1,8 +1,7 @@
 require 'mongo'
 
 client = Mongo::Client.new(['127.0.0.1:27017'], :database => 'local')
-db = client.database
+# db = client.database
 topics = client[:topics]
 topics.insert_one({name: 'john', desc: 'happy'})
-topics.find.all
-topics.find.to_a
+topics.find.to_a.first
